ba119362a24426b3b062820fdced0baaadb31741
Alina <alinayanzorkina@gmail.com>
2022-12-08 17:22:51 +0300

    7 commit

--- memoapp/tests.py
+++ memoapp/tests.py
@@ -1,3 +1,5 @@
-from django.test import TestCase
+import requests
 
-# Create your tests here.
+response = requests.post('http://127.0.0.1:8000/api-token-auth/', data={'username':'user2', 'password': 'r5yrq9s4'})
+print(response.status_code)
+print(response.json())
